8b754281c3677581712d2a607da605c533dd9cf8
Eduardo Mendes <eduardomendes.dev@gmail.com>
2022-05-05 15:37:45 -0300

    first orthodox canonical class implemented

---
+++ MODULE02/ex00/Fixed.cpp
@@ -0,0 +1,38 @@
+#include <Fixed.hpp>
+
+
+Fixed::Fixed(void) {
+	std::cout << "Default constructor called" << std::endl;
+	this->_number = 0;
+	return ;
+}
+
+Fixed::Fixed(Fixed const & src) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = src;
+	return ;
+}
+
+Fixed::~Fixed(void) {
+	std::cout << "Destructor called" << std::endl;
+	return ;
+}
+
+Fixed & Fixed::operator=(Fixed const & src) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->_number = src.getRawBits();
+	return *this;
+}
+
+int     Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return this->_number;
+}
+
+void	Fixed::setRawBits(int const raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	this->_number = raw;
+}
+
+
+int const Fixed::_bits = 8;
---
+++ MODULE02/ex00/Fixed.hpp
@@ -0,0 +1,20 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed {
+    public:
+        Fixed(void);
+        Fixed(Fixed const & src);
+        ~Fixed(void);
+    
+        Fixed & operator=(Fixed const & src);
+        int     getRawBits(void) const;
+        void    setRawBits(int const raw);
+    private: 
+        int _number;
+        int static const _bits;
+};
+
+#endif
