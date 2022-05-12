adf154189ce9ea53e79e45309fdace1d40a1d364
Eduardo Mendes <eduardomendes.dev@gmail.com>
2022-05-12 10:58:36 -0300

    refactor: making the main simplier

--- MODULE01/ex00/main.cpp
+++ MODULE01/ex00/main.cpp
@@ -1,17 +1,11 @@
 #include <Zombie.hpp>
 
 int main(void) {
-	Zombie ze("Zé");
 	Zombie *Marcelo;
-	Zombie *Bub;
 
-	ze.Announce();
 	Marcelo = newZombie("Marcelo");
 	Marcelo->Announce();
-	Bub = newZombie("Bub");
-	Bub->Announce();
 	randomChump("Godofredo");
 	delete Marcelo;
-	delete Bub;
 	return 0;
 }
